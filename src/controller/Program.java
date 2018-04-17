5fd00483e42cc036b9294bfedd138e110b5385b7
alena424 <xtesar36@stud.fit.vutbr.cz>
2018-04-17 13:12:14 +0200

    Add files via upload

--- src/controller/Program.java
+++ src/controller/Program.java
@@ -3,14 +3,37 @@ package controller;
 import model.AdditionBlock;
 import model.Block;
 import model.Scheme;
+import view.DashBoard;
+import view.DashBoard_GUI;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class Program {
 
     public static void main( String[] args ) throws IOException {
-        System.out.println("Enter a password: ");
+
+        // CREATING BLOCKS
+        Scheme newScheme = new Scheme(1);
+        newScheme.factorBlock("Start");
+        newScheme.factorBlock("End");
+
+        newScheme.factorBlock("Addition");
+        newScheme.factorBlock("Invert");
+        newScheme.factorBlock("Random");
+        newScheme.factorBlock("Multiplication");
+        newScheme.factorBlock("Division");
+;
+
+        // DASHBOARD with blocks
+        DashBoard dashboard = new DashBoard(newScheme.getFactoreBlock());
+        dashboard.setVisible();
+
+        // get all blocks generated
+
+
+        /*System.out.println("Enter a password: ");
 
         Scheme scheme = new Scheme( 1 );
         AdditionBlock first = new AdditionBlock( scheme, 0);
@@ -37,9 +60,9 @@ public class Program {
         System.out.println("second: " + second.getLevel() );
         System.out.println("third: " + third.getLevel() );
         System.out.println("cycle: " + scheme.isCycle() );
-        System.out.println("levelFault: " + scheme.isLevelFault() );
-/*
-        HashMap<Integer,Block> map = new HashMap<Integer, Block>();
+        System.out.println("levelFault: " + scheme.isLevelFault() );*/
+
+       /* HashMap<Integer,Block> map = new HashMap<Integer, Block>();
         map.put( 'Alena', 42.0 );
         map.put( 'Alena', 42.0 );
         first.setInput( map, 0 );
