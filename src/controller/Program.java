5adb7115a332b76fedff76e03564d19f13261f88
alena424 <xtesar36@stud.fit.vutbr.cz>
2018-04-23 22:34:26 +0200

    Add files via upload

--- src/controller/Program.java
+++ src/controller/Program.java
@@ -1,24 +1,25 @@
 package controller;
 
-import model.AdditionBlock;
 import model.Block;
 import model.FactoryBlock;
 import model.Scheme;
-import org.w3c.dom.events.Event;
+import view.Connection;
 import view.DashBoard;
-import view.DashBoard_GUI;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 
+import static java.lang.Math.abs;
+
 public class Program {
-    //List <Block> factoreBlocks = new ArrayList<>();
 
     Scheme newScheme = new Scheme(1); // new shceme
     DashBoard dashboard; // new dashboard
@@ -28,7 +29,8 @@ public class Program {
     List<view.AbstractBlock> viewWorkingBlocks; // blocks on main canvas to work with in VIEW
     List<model.Block> modelWorkingBlocks; // blocks on main canvas to work with in MODEL
 
-    List<view.AbstractBlock> selectedBlock; //selected blocks
+    List<view.AbstractBlock> selectedBlock; //selected blocks, maximum is 2 blocks
+    List<Connection> connectionsList = new ArrayList<>();
 
     public Program(){
 
@@ -63,20 +65,20 @@ public class Program {
             }
         });
 
+        // created default blocks we can use
         factoreBlocks = new ArrayList<>( newScheme.getFactoreBlock() );
         System.out.println(factoreBlocks);
-
+        // put blocks on left panel1
         dashboard.getBlocksOnLeft(newScheme.getFactoreBlock());
-
-
-
+        // save blocks in left panel in panelBlocks
         panelBlocks = dashboard.getBlocksOnPanel();
+
+        //initialize array of view and model blocks
         viewWorkingBlocks = new ArrayList<>();
         modelWorkingBlocks = new ArrayList<>();
 
+        // special inicialization for start and end block
         initFactoryBlock(newScheme.getfactoreBlockByName("Start"), newScheme.getfactoreBlockByName("End"));
-
-
 
         for (JLabel lab: panelBlocks
                 ) {
@@ -97,19 +99,47 @@ public class Program {
     }
     protected void deleteModelBlock(model.Block block){
         System.out.println("deleting model ");
-
         modelWorkingBlocks.remove(block);
         block = null;
-
     }
 
+    /**
+     * Method deletes a view block with all connections connected to block
+     * @param block
+     */
     protected void deleteViewBlock(view.AbstractBlock block){
         System.out.println("deleting label view");
         dashboard.panel2.remove(block.labelBlock);
+
+        // delete connection connected to block if there is just one
+        deleteConnection(block);
         dashboard.panel2.validate();
         dashboard.panel2.repaint();
         viewWorkingBlocks.remove(block);
         block = null;
+    }
+
+    /**
+     * Method deletes all connections relative to block
+     * @param block
+     */
+    protected void deleteConnection(view.AbstractBlock block){
+       // System.out.println(dashboard.getConnectionsOnDashboard());
+        List<Connection> toRemoveConnection = new ArrayList<>();
+        for (Connection i: dashboard.getConnectionsOnDashboard()
+             ) {
+            if ( i.getBlockTo() == block || i.getBlockFrom() == block){
+
+                toRemoveConnection.add(i);
+
+            }
+        }
+        for ( Connection r: toRemoveConnection
+             ) {
+            dashboard.getConnectionsOnDashboard().remove(r);
+            r = null;
+        }
+        dashboard.panel2.repaint();
     }
 
     /**
@@ -126,8 +156,8 @@ public class Program {
     }
 
     /**
-     * Function generates block of type block
-     * @param block block we get information from when we re creating view and model bblock
+     * Function generates block
+     * @param block block we get information from, when we are creating view and model block
      */
     public void generateBlock(Block block){
         // new label for new block
@@ -140,21 +170,55 @@ public class Program {
         // CREATING VIEW BLOCK
         view.AbstractBlock addViewBlock;
         if ( block.static_block ){
+            // we get static coordinates from model
             addViewBlock = new view.StaticBlock(block.getName(), lblock);
             if ( block.static_x != 0 && block.static_y != 0){
                 addViewBlock.setX(block.static_x);
                 addViewBlock.setY(block.static_y);
-
             } else {
                 System.err.println("Static blocks must contain their static coordinates\n");
                 return;
             }
-
         } else {
             addViewBlock = new view.Block(block.getName(), lblock);
+            // drag and drop
+            lblock.addMouseMotionListener(new MouseAdapter() {
+                @Override
+                public void mouseDragged(MouseEvent event) {
+                    //super.mouseDragged(e);
+                    System.out.println("Dragged");
+                    if ( event.getComponent().getY() <= 0 && event.getComponent().getX() <= 0 ){
+                        event.getComponent().setLocation(abs(event.getX()+event.getComponent().getX()-addViewBlock.getXBlock()),
+                                abs(event.getY()+event.getComponent().getY()-addViewBlock.getYBlock()) );
+                    } else if ( event.getComponent().getX() > 660 ) {
+                        event.getComponent().setLocation(660,   event.getY()+event.getComponent().getY()-addViewBlock.getYBlock());
+                    } else if ( event.getComponent().getY() > 550 ) {
+                        event.getComponent().setLocation(event.getX()+event.getComponent().getX()-addViewBlock.getXBlock(),   550);
+                    } else {
+                        event.getComponent().setLocation(abs(event.getX()+event.getComponent().getX()-addViewBlock.getXBlock()),
+                                abs(event.getY()+event.getComponent().getY()-addViewBlock.getYBlock()) );
+                    }
+                }
+
+            });
+            // after release, we need to update coordinates of port and connections
+            lblock.addMouseListener(new MouseAdapter() {
+                  @Override
+                  public void mouseReleased(MouseEvent event) {
+                      System.out.println("release");
+                      addViewBlock.setX( event.getX()+event.getComponent().getX()-addViewBlock.getXBlock() );
+                      addViewBlock.setY( event.getY()+event.getComponent().getY()-addViewBlock.getYBlock() );
+
+
+                      if ( ! dashboard.getConnectionsOnDashboard().isEmpty() ){
+                          actualizeAllConnection();
+                          System.out.println("Actualize");
+                          dashboard.panel2.repaint();
+                      }
+                  }
+              });
         }
 
-        System.out.println(addViewBlock);
         viewWorkingBlocks.add(addViewBlock);
 
         // CREATING MODEL BLOCK
@@ -176,16 +240,14 @@ public class Program {
         addViewBlock.setSelectedPath(block.getSelectedIcon());
 
         addViewBlock.labelBlock.setIcon(new ImageIcon( addViewBlock.getPath() ) );
-        System.out.println(addViewBlock.getPath());
 
         dashboard.panel2.add(addViewBlock.labelBlock);
         if ( block.static_block ){
-            // if it is a static block, can not be moved and it has special coorinates
+            // if it is a static block, can not be moved and it has special coordinates
             addViewBlock.labelBlock.setBounds(block.static_x, block.static_y, addViewBlock.getWidthBlock(), addViewBlock.getHeightBlock());
         } else {
             addViewBlock.labelBlock.setBounds(addViewBlock.getXBlock(), addViewBlock.getYBlock(), addViewBlock.getWidthBlock(), addViewBlock.getHeightBlock());
         }
-
 
         lblock.addMouseListener(new MouseAdapter() {
             @Override
@@ -195,7 +257,7 @@ public class Program {
                     System.out.println("right click");
                     int indexM = modelWorkingBlocks.indexOf(addModelBlock);
                     if ( indexM != -1 ){
-                        // cano not delete static objects!
+                        // can not delete static objects!
                         if ( ! modelWorkingBlocks.get(indexM).static_block )
                                 deleteModelBlock( modelWorkingBlocks.get(indexM));
                     } else {
@@ -223,63 +285,85 @@ public class Program {
                             makeNewConnection(e);
                         }
                     } else {
-                        // if we have 2 nad more, it should not happen and we clear list
+                        // if we have 2 and more, it should not happen and we clear list
                         distroySelectedBlocks();
                     }
-
                 }
             }
         });
     }
-    public void drawPoint(Point a){
-        Graphics g = dashboard.panel2.getGraphics();
-        Graphics2D g2d = (Graphics2D)g;
 
-        //g2d.setStroke(new BasicStroke(10));
-        g2d.drawLine(a.x, a.y, a.x, a.y);
+    /**
+     * Method actualizes all connection
+     * all connections are found in dashboard.getConnectionsOnDashboard()
+     */
+    public void actualizeAllConnection (){
+        for ( Connection i: dashboard.getConnectionsOnDashboard()
+             ) {
+            i.actualize();
+        }
     }
 
+    /**
+     * Method makes new connection
+     * 1. gets points where was clicked
+     * 2. gets type of ports (input, output), 2 selected points can not have same port
+     * 3. gets id of port
+     * 4. from id we get the point of right port
+     * @param event click point
+     */
     public void makeNewConnection(MouseEvent event){
         // we have 2 blocks to connect
-       Graphics g = dashboard.panel2.getGraphics();
-        Graphics2D g2d = (Graphics2D)g;
-        int width = 4;
-        g2d.setStroke(new BasicStroke(width));
-
-        g2d.setColor(new Color(61,204,199));
-        System.out.println("Block1 " + selectedBlock.get(0).getPath() + ": x = " + selectedBlock.get(0).getXBlock() + "y = " +selectedBlock.get(0).getYBlock());
-        System.out.println("Block2 "+ selectedBlock.get(1).getPath() + ":  x = " + selectedBlock.get(1).getXBlock() + "y = " +selectedBlock.get(1).getYBlock());
 
         Point firstPoint = new Point(
-                selectedBlock.get(0).getLeftClickedLast().x + selectedBlock.get(1).getXBlock(),
-                selectedBlock.get(0).getLeftClickedLast().y +selectedBlock.get(1).getYBlock());
-        drawPoint(firstPoint);
+                selectedBlock.get(0).getLeftClickedLast().x + selectedBlock.get(0).getXBlock(),
+                selectedBlock.get(0).getLeftClickedLast().y +selectedBlock.get(0).getYBlock());
+        System.out.println("First point:");
+        System.out.println(firstPoint);
 
         Point clickPoint = new Point(
-                event.getX()+selectedBlock.get(1).getXBlock(),
-                event.getY()+selectedBlock.get(1).getYBlock());
-        drawPoint(clickPoint);
+                event.getX() +selectedBlock.get(1).getXBlock(),
+                event.getY() +selectedBlock.get(1).getYBlock());
 
-        System.out.println("Fist click:");
-        System.out.println(firstPoint);
+        System.out.println("Second point:");
+        System.out.println(clickPoint);
 
         System.out.println("Clicked place:");
         System.out.println(clickPoint);
-        //Point portfirst = selectedBlock.get(0).getPort(selectedBlock.get(0).getLeftClickedLast());
-        Point portfirst = selectedBlock.get(0).getPort(firstPoint);
-        Point portsecond = selectedBlock.get(1).getPort(clickPoint);
-        drawPoint(portfirst);
-        drawPoint(portsecond);
-        System.out.println("Port on second block found:");
-        System.out.println(portsecond);
 
-        //g2d.drawLine(selectedBlock.get(0).getXBlock(), selectedBlock.get(0).getYBlock(), selectedBlock.get(1).getXBlock(), selectedBlock.get(1).getYBlock());
-        //g2d.drawLine(selectedBlock.get(0).getXBlock(), selectedBlock.get(0).getYBlock(), selectedBlock.get(1).getXBlock(), selectedBlock.get(1).getYBlock());
-        //g2d.drawLine(0,0,500,250);
-        g2d.drawLine(portfirst.x,portfirst.y, portsecond.x, portsecond.y);
+        Integer typePortFirst = selectedBlock.get(0).getTypePort(firstPoint);
+        Integer typePortSecond  = selectedBlock.get(1).getTypePort(clickPoint);
+
+        if ( typePortFirst == typePortSecond ){
+            System.err.println("You clicked on same type of port, port must be different type");
+
+        } else {
+            Integer idFirst = selectedBlock.get(0).getPortId(firstPoint,typePortFirst);
+            Integer idSecond = selectedBlock.get(1).getPortId(clickPoint, typePortSecond);
+
+            //Point portfirst = selectedBlock.get(0).getPort(idFirst, typePortFirst);
+            //Point portsecond = selectedBlock.get(1).getPort(idSecond,typePortSecond);
+            // make new connection
+            Connection conn = new Connection(selectedBlock.get(0), selectedBlock.get(1));
+            // set id of ports
+            conn.setIdFromPort(idFirst);
+            conn.setIdToPort(idSecond);
+            // set types of ports
+            conn.setTypeFrom(typePortFirst);
+            conn.setTypeTo(typePortSecond);
+            // actualize coordinates of ports
+            conn.actualize();
+
+            dashboard.addConnectionsOnDashboard(conn);
+        }
+
         distroySelectedBlocks();
+
     }
 
+    /**
+     * Method clears selectedBlock array and change icon to normal
+     */
     public void distroySelectedBlocks(){
         for (view.AbstractBlock vblock : selectedBlock){
             changeIconToNormal(vblock);
@@ -301,12 +385,14 @@ public class Program {
         return null;
     }
 
+    /**
+     * Main runs program
+     * @param args
+     * @throws IOException
+     */
     public static void main( String[] args ) throws IOException {
         Program program = new Program();
         program.run();
 
     }
-
-
-
 }
